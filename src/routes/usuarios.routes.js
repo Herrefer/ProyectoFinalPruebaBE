d6aae261bead8a203c67cfd21b84bc5eef8984ae
Fernando Herrera <herrera770@outlook.es>
2024-04-11 10:01:04 -0300

    se agregaron las rutas de validaciones nen usuarios.routes y las logicas de login

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { crearUsuario, leerUsuarios, login } from '../controllers/usuarios.controllers.js';
+import { crearUsuario, login } from '../controllers/usuarios.controllers.js';
+import validacionRegistro from '../helpers/validacionesDeRegistro.js';
+import validacionLogin from '../helpers/validacionDeLogin.js';
 
 
 const enrutador = Router();
 
-enrutador.route('/registro').post(crearUsuario).get(leerUsuarios);
-enrutador.route('/login').post(login).get(leerUsuarios);
+enrutador.route('/registro').post([validacionRegistro],crearUsuario);
+enrutador.route('/login').post([validacionLogin],login);
 
 export default enrutador;
