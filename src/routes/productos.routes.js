792b3ec0b9aa4b88c15bb1cde61699aaff2dcf0d
Fernando Herrera <herrera770@outlook.es>
2024-04-14 11:56:19 -0300

    se agregó una validacion de JWT en la funcion de crearProducto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -6,10 +6,11 @@ import {
   borrarProducto,
   editarProducto,
 } from '../controllers/productos.controllers.js';
+import validarJWT from '../helpers/verificarJWT.js';
 
 const enrutador = Router();
 
-enrutador.route('/productos').get(listarProductos).post(crearProducto);
+enrutador.route('/productos').get(listarProductos).post([validarJWT],crearProducto);
 enrutador.route('/producto/:id').get(obtenerProducto).delete(borrarProducto).put(editarProducto);
 
 export default enrutador;
